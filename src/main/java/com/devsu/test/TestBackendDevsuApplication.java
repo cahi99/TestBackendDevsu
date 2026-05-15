b4f2915a967babc2991b72b6adaf53fcfb0bcd1c
cahi99 <cris.c.a.h.i99@gmail.com>
2026-05-15 08:09:07 -0500

    chore(config): inicializar proyecto Spring Boot 3 con dependencias y H2

---
+++ src/main/java/com/devsu/test/TestBackendDevsuApplication.java
@@ -0,0 +1,12 @@
+package com.devsu.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBackendDevsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBackendDevsuApplication.class, args);
+    }
+}
